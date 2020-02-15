46b24300000004546fff43ac54186dbf35900403
BruceYangTP <bruce.yang@thinkpower-info.com>
2020-02-15 15:19:23 +0800

    add Vue in html and css

---
+++ vue/script.js
@@ -0,0 +1,12 @@
+const vm = new Vue ({
+    el:'#app',
+    data:{
+        name:'Bruce Yang',
+        title:'-ThinkPower Engineer',
+        date:'2019 / 2 / 15',
+        cardInfoStyle:'card-text-info',
+        imgSrc:'images/personal.png',
+        bgColor:'cardBlue'
+    }
+})
+
